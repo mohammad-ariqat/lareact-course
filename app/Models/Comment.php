5ce31708c8b62a6f88fdaa10816cbac042725931
Mo.Araiqat <107495343+mohammad-ariqat@users.noreply.github.com>
2025-02-13 22:08:53 +0300

    Created models and migrations and relationships

---
+++ app/Models/Comment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    /**
+     * Get the user that owns the Comment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function feature(): BelongsTo
+    {
+        return $this->belongsTo(Feature::class);
+    }
+}
---
+++ app/Models/Feature.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Feature extends Model
+{
+    /**
+     * Get all of the upvotes for the Feature
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function upvotes(): HasMany
+    {   
+        return $this->hasMany(Upvote::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
